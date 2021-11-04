c312a2e56e6afb430b2c82e502339642e44219ad
Maxim Samsonenko <samsonenko.max@gmail.com>
2021-11-04 20:51:15 +0200

    adds comments to task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,9 +1,14 @@
+//get access to the list with id '#categories'
 const categories = document.querySelector("#categories");
+
+//create pseudo array of the list items
 const categoryItems = categories.children;
 
+//show in console number of categories (li items in the list)
 const showCategories = (pseudoArray) =>
 	console.log(`Number of categories: ${pseudoArray.length}`);
 
+//get access to header text and category list length and print it in console
 const showItems = (pseudoArray) => {
 	for (const el of pseudoArray) {
 		const header = el.querySelector("h2").textContent;
@@ -13,5 +18,6 @@ const showItems = (pseudoArray) => {
 	}
 };
 
+//function calls
 showCategories(categoryItems);
 showItems(categoryItems);
