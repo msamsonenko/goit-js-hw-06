af67f2f7082dc30e007f2ef417465dffe86b19a4
Maxim Samsonenko <samsonenko.max@gmail.com>
2021-11-04 21:21:43 +0200

    completes task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,33 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+	"Potatoes",
+	"Mushrooms",
+	"Garlic",
+	"Tomatos",
+	"Herbs",
+	"Condiments",
 ];
+
+//get access to the list "ingredients"
+const ingredientsList = document.querySelector("#ingredients");
+
+//create li item, add text content, add class
+const createListItem = (array) => {
+	const list = [];
+	array.forEach((arr) => {
+		const element = document.createElement("li");
+		element.textContent = arr;
+		element.classList.add("item");
+		list.push(element);
+	});
+
+	return list;
+};
+
+//create list of li items
+const listItems = createListItem(ingredients);
+
+//adds array of children items to a parent
+const addListItems = (list, items) => list.append(...items);
+
+//call function to add items (li) to list (ul)
+addListItems(ingredientsList, listItems);
